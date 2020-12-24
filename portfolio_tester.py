b70133fdc373a4af94cf02b2a3607651caadf387
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-12-24 01:03:02 -0500

    daily returns for 100 random portfolios

--- portfolio_tester.py
+++ portfolio_tester.py
@@ -5,4 +5,12 @@ portfolios = [Portfolio() for x in range(100)]
 
 df = pd.DataFrame([p.holdings for p in portfolios])
 df.to_pickle("test.pkl")
-print(df)
+
+numPort = 1
+
+for p in portfolios:
+    returnsName = "portfolio"+str(numPort)+"Returns.pkl"
+    dailyReturns = p.get_equity_data()
+    dailyReturns.to_pickle(returnsName)
+    numPort += 1
+
--- tearSheet.py
+++ tearSheet.py
@@ -1,4 +1,13 @@
-from finquant.portfolio import build_portfolio
+import numpy as np
+from empyrical import max_drawdown, alpha_beta
+import warnings
+warnings.filterwarnings('ignore')
 
-names = ['WIKI/WMT', 'WIKI/AMZN']
-pf = build_portfolio(names=names)
+returns = np.array([.01, .02, .03, -.4, -.06, -.02])
+benchmark_returns = np.array([.02, .02, .03, -.35, -.05, -.01])
+
+# calculate the max drawdown
+print(max_drawdown(returns))
+
+# calculate alpha and beta
+alpha, beta = alpha_beta(returns, benchmark_returns)
