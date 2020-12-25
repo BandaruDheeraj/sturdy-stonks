65b7a0b9f7bfcf5df2d6643a3a352396f321eec7
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-12-24 23:51:47 -0500

    Best max drawdown calculation

--- tearSheet.py
+++ tearSheet.py
@@ -1,13 +1,27 @@
 import numpy as np
+import pandas as pd
 from empyrical import max_drawdown, alpha_beta
 import warnings
 warnings.filterwarnings('ignore')
 
-returns = np.array([.01, .02, .03, -.4, -.06, -.02])
-benchmark_returns = np.array([.02, .02, .03, -.35, -.05, -.01])
+bestPortfolio = 0
+lowestDrawdown = float('inf') * -1
 
-# calculate the max drawdown
-print(max_drawdown(returns))
+for i in range(1,100):
+    returnsName = "portfolio"+str(i)+"DailyPercentChange.pkl"
+    returns = pd.read_pickle(returnsName)
+
+    # calculate the max drawdown
+    if max_drawdown(returns).mean() > lowestDrawdown:
+        bestPortfolio = i
+        lowestDrawdown = max_drawdown(returns).mean() 
+
+print(lowestDrawdown)
+print(bestPortfolio)
+
+bestPortHoldings = "portfolio"+str(bestPortfolio)+"DailyPercentChange.pkl"
+bestPortHoldings = pd.read_pickle(bestPortHoldings)
+print(bestPortHoldings)
 
 # calculate alpha and beta
-alpha, beta = alpha_beta(returns, benchmark_returns)
+# alpha, beta = alpha_beta(returns, benchmark_returns)
