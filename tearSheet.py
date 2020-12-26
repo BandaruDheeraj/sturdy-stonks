5bff3b46560d4ce80e2ca87d0be9b60d47ee1189
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-12-25 21:02:27 -0500

    replaced missing data with 0.0

--- tearSheet.py
+++ tearSheet.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from empyrical import max_drawdown, alpha_beta, sharpe_ratio, tail_ratio
+from empyrical import max_drawdown, alpha_beta, sharpe_ratio, tail_ratio, omega_ratio
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -10,14 +10,19 @@ lowestDrawdown = float('inf') * -1
 highestSharpeRatio = float('inf') * -1
 bestTail = 0
 highestTailRatio = float('inf')
+bestOmega = 0
+highestOmegaRatio = float('inf') * -1
 
 for i in range(1,100):
     returnsName = "portfolio"+str(i)+"DailyPercentChange.pkl"
     returns = pd.read_pickle(returnsName)
+    returns = returns[1:]
+    returns = returns.fillna(0.0)
 
     drawDown = max_drawdown(returns).mean()
     sharpe = sharpe_ratio(returns).mean() # risk free = 0, period = daily
     tail = tail_ratio(returns).mean()
+    # omega = omega_ratio(returns)
     # calculate the max drawdown
     if  drawDown > lowestDrawdown:
         bestDrawdown = i
@@ -32,6 +37,11 @@ for i in range(1,100):
     if tail < highestTailRatio:
         bestTail = i
         highestTailRatio = tail
+
+    # calculate the omega ratio, required return 100
+    # if omega > highestOmegaRatio:
+    #     bestOmega = i
+    #     highestOmegaRatio = omega
     
 
 
@@ -45,11 +55,14 @@ print(bestSharpe)
 print(highestTailRatio)
 print(bestTail)
 
+print(highestOmegaRatio)
+print(bestOmega)
 
 
-# bestPortHoldings = "portfolio"+str(bestSharpe)+"DailyPercentChange.pkl"
-# bestPortHoldings = pd.read_pickle(bestPortHoldings)
-# print(bestPortHoldings)
+
+bestPortHoldings = "portfolio"+str(bestSharpe)+"DailyPercentChange.pkl"
+bestPortHoldings = pd.read_pickle(bestPortHoldings)
+print(bestPortHoldings)
 
 
 #need benchmark for alpha beta calculations
